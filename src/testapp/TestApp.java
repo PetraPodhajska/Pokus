171679d2f2bd571e337d587f17a94ea216d24948
petra.podhajska <petra.podhajska@B110-07.pslib.cz>
2016-05-11 09:17:10 +0200

    doplnéní druhého hlášení

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -16,6 +16,8 @@ public class TestApp {
      */
     public static void main(String[] args) {
         System.out.println("Ahoj světe");
+        System.out.println("Jak se máš?");
+        
     }
     
 }
